c56bcacd9ed1709d0a2e3a03e9ee051f31955b0b
amanuel12345a <am@nuelsew@gegn@gmail.com>
2023-06-12 21:11:54 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const userRoutes = require("./routes/UserRoutes");
+const mongoose = require("mongoose");
+const env = require('dotenv').config()
+const PORT = process.env.PORT || 8081;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGODB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connetion Successfull");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+app.use("/api/user", userRoutes);
+
+app.listen(PORT, () => {
+  console.log("server started on port 5000");
+});
