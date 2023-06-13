07a1d01ded9c81ab176cbd48ce0e8c2ba01788bf
amanuel12345a <am@nuelsew@gegn@gmail.com>
2023-06-12 21:31:07 -0500

    edit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const userRoutes = require("./routes/UserRoutes");
+const userRoutes = require("./routes/UserRouter");
 const mongoose = require("mongoose");
 const env = require('dotenv').config()
 const PORT = process.env.PORT || 8081;
